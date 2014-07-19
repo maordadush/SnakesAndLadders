293281b7d9263a5917661801a2026e15f10b3f2f
maordadush <maordadush@gmail.com>
2014-07-19 20:29:02 +0300

    Add ShuffleSnakeAndLadders

--- src/snakesandladders/gamemodel/SnakesAndLaddersSingleGame.java
+++ src/snakesandladders/gamemodel/SnakesAndLaddersSingleGame.java
@@ -7,6 +7,7 @@ package snakesandladders.gamemodel;
 
 import java.util.Random;
 import snakesandladders.players.aPlayer;
+import sun.nio.cs.ext.ExtendedCharsets;
 
 /**
  *
@@ -15,16 +16,16 @@ import snakesandladders.players.aPlayer;
 public class SnakesAndLaddersSingleGame implements iWinChecker {
 
     private int m_BoardSize = 0;
-    private final eChars m_GameBoard[][];
+    private BoardSquare m_GameBoard[][];
     private int gameWinner;
 
     public SnakesAndLaddersSingleGame(int o_BoardSize) {
-        if (o_BoardSize > 4 || o_BoardSize < 8) {
+        if (o_BoardSize > 4 || o_BoardSize < 9) {
             m_BoardSize = o_BoardSize;
-            m_GameBoard = new eChars[m_BoardSize][m_BoardSize];
+            m_GameBoard = new BoardSquare[m_BoardSize][m_BoardSize];
         } else {
             throw new UnsupportedOperationException("Illeagal board size"); //To change body of generated methods, choose Tools | Templates.
-
+            
         }
 
         gameWinner = -1;
@@ -37,16 +38,37 @@ public class SnakesAndLaddersSingleGame implements iWinChecker {
     public void initGame() {
         for (int i = 0; i < m_BoardSize; i++) {
             for (int j = 0; j < m_BoardSize; j++) {
-                m_GameBoard[i][j] = eChars.NONE;
+                m_GameBoard[i][j] = new BoardSquare();
             }
         }
         gameWinner = -1;
     }
 
     public void shuffleSnakesAndLadders(int o_NumOfSnakesAndLadders) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Random rand = new Random();
+        for (int i = 0; i < o_NumOfSnakesAndLadders; i++) {
+            int X = rand.nextInt(this.m_BoardSize - 1);
+            int Y = rand.nextInt(this.m_BoardSize - 1);
+            while ((X != 0 && Y != 0) && (X != m_BoardSize - 1 && Y != m_BoardSize - 1)
+                    && (m_GameBoard[X][Y].getType() != eChars.NONE)) {
+                X = rand.nextInt(this.m_BoardSize - 1);
+                Y = rand.nextInt(this.m_BoardSize - 1);
+            }
+            int nextX = rand.nextInt(m_BoardSize - X - 1);
+            int nextY = rand.nextInt(m_BoardSize - 1);
+            while ((X != 0 && Y != 0) && (m_GameBoard[X][Y].getType() != eChars.NONE)) {
+                nextX = rand.nextInt(this.m_BoardSize - X - 1);
+                nextY = rand.nextInt(this.m_BoardSize - 1);
+            }
+            
+            // set snake paramter tail
+            m_GameBoard[X][Y].setType(eChars.SNAKE);
+            m_GameBoard[nextX][nextY].setType(eChars.SNAKE);
+            m_GameBoard[nextX][nextY].setType(eChars.SNAKE);
+            m_GameBoard[X][Y].JumpTo.setLocation(nextX, nextY);
+              
+        }
     }
-
     private int throwCube() {
         Random rand = new Random();
 
